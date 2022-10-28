70111452e21d44967044548644747d9d210234ab
Danin Sadžak <daninsadzak@gmail.com>
2022-10-28 22:42:11 +0200

    Added the title screen

---
+++ app.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+/*
+    Author: Danin Sadzak (@Neoxelly)
+*/
+int main(){
+    std::cout<<"************************************\n";
+    std::cout<<"*   Danin's Minigame Collection!   *\n";
+    std::cout<<"*                                  *\n";
+    std::cout<<"*   Press Any Button To Continue   *\n";
+    std::cout<<"************************************\n";
+    return 0;
+}
