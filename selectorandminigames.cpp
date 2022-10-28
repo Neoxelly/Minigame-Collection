df331e22b13564fda0cdfdf19f95331c3fdee9b2
Danin Sadžak <daninsadzak@gmail.com>
2022-10-29 01:02:58 +0200

    Added Number guessing game fucntion

--- selectorandminigames.cpp
+++ selectorandminigames.cpp
@@ -10,5 +10,25 @@ void selector(){
     std::cout<<"5. Snake\n";
     std::cout<<"6. Minesweeper\n";
     std::cout<<"7. Sudoku\n";
-    std::cout<<"8. Chess\n";
+    std::cout<<"8. Chess\n\n";
+    std::cout<<"(Note: To select use numbers from 1-8!)\n\n";
+    int chooseMinigame;
+    std::cin>>chooseMinigame;
+    switch(chooseMinigame){
+        case 1:
+            numberGuessingGame();
+            break;
+    }
+}
+
+void numberGuessingGame(){
+    system("CLS");
+    int computerOrPlayer;
+    std::cout<<"***********************"<<'\n';
+    std::cout<<"*Number guessing game!*"<<'\n';
+    std::cout<<"***********************"<<'\n';
+    std::cout<<"Would you like to guess the number or would you like the computer to guess your number?\n";
+    std::cout<<"1. I want to guess the number!\n";
+    std::cout<<"2. I want the computer to guess my number!\n";
+    std::cin>>computerOrPlayer;
 }
